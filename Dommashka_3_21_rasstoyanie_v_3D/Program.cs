66c453e0e684d08229976b6397104a9c509d4efb
Vlad <vlad_zaycev_1984@mail.ru>
2022-10-24 00:25:38 +0300

    расстоние между двум точками

---
+++ Dommashka_3_21_rasstoyanie_v_3D/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает координаты 2х точек и 
+// находит расстоние между ними в пространстве 3D
+
+Console.Clear();
+int xa = 3, ya = 6, za = 8,
+    xb = 2, yb = 1, zb = -7;
+
+double distance = Math.Sqrt((Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2)));
+Console.WriteLine($"расстояние между двумя токами {distance}");
