fda5277ce463e4e8c6f89c93fa54e5a7826d2cd0
Vlad <vlad_zaycev_1984@mail.ru>
2022-10-23 22:17:57 +0300

    таблица кубов от 1 до Н

---
+++ Dommashka_3_23_kubi_ot_1_do_N/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int stepen = 3;
+
+for(int i = 1; i < n + 1; i++)
+{  
+   Console.WriteLine(Math.Pow(i, stepen)); 
+}
