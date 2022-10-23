9791ca820b96a74af13f5112694b04caadad92b6
Vlad <vlad_zaycev_1984@mail.ru>
2022-10-23 21:16:46 +0300

    палиндром 5знаного числа

---
+++ Dommashka_3_19_polindrom/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход пятизнаное значение
+// и проверяет является ли оно палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int palindrome = int.Parse(Console.ReadLine()!);
+
+int num5 = palindrome % 10;
+int num1 = palindrome / 10000;
+
+int num4 = palindrome % 100 / 10;
+int num3 = palindrome / 1000 % 10;
+
+if(num5 == num1 && num4 == num3) Console.WriteLine($"число {palindrome} является палиндромом");
+else Console.WriteLine($"число {palindrome} неявляется палиндромом");
+
